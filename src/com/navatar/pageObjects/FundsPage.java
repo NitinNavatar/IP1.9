0ec5c2999eb43027528b63dc87ff7e8efc7aef00
Azhar <navatarip@navatargroup.com>
2020-04-07 18:36:14 +0530

    Updated M8tc035_CheckFunctionalityOfClearButton
    Signed-off-by: Azhar

--- src/com/navatar/pageObjects/FundsPage.java
+++ src/com/navatar/pageObjects/FundsPage.java
@@ -844,7 +844,7 @@ public class FundsPage extends BasePageBusinessLayer {
 		return isDisplayed(driver, FindElement(driver, "//div[@id='"+workSpaceSelector+"workspace']//a[@title='Clear Workspace']","workspace clear button", action.BOOLEAN,timeOut), "visibility", timeOut,"workspace close button");
 	}
 	
-	@FindBy(css="#LinkButton3")
+	@FindBy(xpath="//div[@id='create_savebtn']/a[@title='Yes']")
 	private WebElement CloseAndClearWorkSpaceYesBtn;
 	
 /**
